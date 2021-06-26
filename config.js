c5b1fb10db52057ba7bdedd89bf4cbc73ea44b8e
Rimantas <belauzas@yahoo.com>
2021-06-26 19:41:19 +0300

    Cofig update

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ environments.dev = {
     httpsPort: 3001,
     envName: 'dev',
     hashingSecret: 'development-random-hashing-secret-string',
+    tokenExpirationPeriod: 3600000,
 }
 
 environments.production = {
@@ -12,6 +13,7 @@ environments.production = {
     httpsPort: 5001,
     envName: 'production',
     hashingSecret: 'production-random-hashing-secret-string',
+    tokenExpirationPeriod: 3600000,
 }
 
 const currentEnv = typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV.toLowerCase() : '';
